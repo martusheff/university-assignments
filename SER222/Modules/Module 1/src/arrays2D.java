cdfaabb6a23055ca567465752a42c03e960c1bab
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-21 14:53:08 -0700

    SER222 - Module 1 - Deliverable 1, 2d arrays syntax review

---
+++ SER222/Modules/Module 1/src/arrays2D.java
@@ -0,0 +1,19 @@
+public class arrays2D {
+    public static void main(String[] args) {
+        int[][] lotteryCard = {
+                {20, 15, 7},
+                {8, 7, 19},
+                {7, 13, 47}
+        };
+
+
+
+        for (int i = 0; i < lotteryCard.length; i++) {
+            for (int j = 0; j < lotteryCard[i].length; j ++) {
+                System.out.print(lotteryCard[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
