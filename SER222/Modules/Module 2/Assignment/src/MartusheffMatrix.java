39359a04f42723f5afcdf610f6c80ae60aeabb86
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-25 14:02:20 -0700

    SER222 - Deliverable 2, completed, reference error resolved

--- SER222/Modules/Module 2/Assignment/src/MartusheffMatrix.java
+++ SER222/Modules/Module 2/Assignment/src/MartusheffMatrix.java
@@ -1,16 +1,32 @@
 /**
  * An implementation of the Matrix ADT. Provides four basic operations over an
  * immutable type.
- * 
- * @author (your name), Ruben Acuna
- * @version (version)
+ *
+ * @author Andronick Martusheff, Ruben Acuna
+ * @version v8.25.2021
  */
+
 public class MartusheffMatrix implements Matrix {
 
-    private int[][] data;
+    private int[][] matrix;
+    private int rows = 0;
+    private int cols = 0;
 
     private MartusheffMatrix(int[][] data) {
-        this.data = data;
+        if (data.length == 0) {
+            rows = 0;
+            cols = 0;
+        } else {
+            rows = data.length;
+            cols = data[0].length;
+        }
+
+        matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = data[i][j];
+            }
+        }
     }
 
 
@@ -18,16 +34,16 @@ public class MartusheffMatrix implements Matrix {
 
     @Override
     public int getElement(int y, int x) {
-        return data[y][x];
+        return matrix[y][x];
     }
 
     @Override
     public int getRows() {
-        if (data.length == 0)
+        if (matrix.length == 0)
             return 0;
 
         int counter = 0;
-        for (int i = 0; i < data.length; i++)
+        for (int i = 0; i < matrix.length; i++)
             counter++;
         return counter;
     }
@@ -35,12 +51,12 @@ public class MartusheffMatrix implements Matrix {
 
     @Override
     public int getColumns() {
-        if (data.length == 0)
+        if (matrix.length == 0)
             return 0;
 
         int counter = 0;
 
-        for (int i = 0; i < data[0].length; i++)
+        for (int i = 0; i < matrix[0].length; i++)
             counter++;
 
         return counter;
@@ -49,8 +65,6 @@ public class MartusheffMatrix implements Matrix {
     @Override
     public Matrix scale(int scalar) {
 
-        int rows = this.getRows();
-        int cols = this.getColumns();
         int[][] data = new int[rows][cols];
 
         for (int i = 0; i < rows; i++) {
@@ -64,8 +78,7 @@ public class MartusheffMatrix implements Matrix {
 
     @Override
     public Matrix plus(Matrix other) {
-        int rows = this.getRows();
-        int cols = this.getColumns();
+
         int[][] data = new int[rows][cols];
 
         if ((cols == other.getColumns()) && (rows == other.getRows())) {
@@ -84,8 +97,7 @@ public class MartusheffMatrix implements Matrix {
 
     @Override
     public Matrix minus(Matrix other) {
-        int rows = this.getRows();
-        int cols = this.getColumns();
+
         int[][] data = new int[rows][cols];
 
         if ((cols == other.getColumns()) && (rows == other.getRows())) {
@@ -98,14 +110,13 @@ public class MartusheffMatrix implements Matrix {
             return null;
         }
 
-        Matrix minus = new MartusheffMatrix(data);
-        return minus;
+        return new MartusheffMatrix(data);
+
     }
 
     @Override
     public Matrix multiply(Matrix other) {
-        int rows = this.getRows();
-        int cols = this.getColumns();
+
         int[][] data = new int[rows][cols];
 
         if ((cols == other.getColumns()) && (rows == other.getRows())) {
@@ -126,9 +137,6 @@ public class MartusheffMatrix implements Matrix {
         if (o == null) return false;
         if (o.getClass() != this.getClass()) return false;
 
-        boolean flag;
-        int rows = this.getRows();
-        int cols = this.getColumns();
         if ((cols == ((MartusheffMatrix) o).getColumns()) && (rows == ((MartusheffMatrix) o).getRows())) {
             for (int i = 0; i < this.getRows(); i++) {
                 for (int j = 0; j < this.getColumns(); j++) {
@@ -139,6 +147,7 @@ public class MartusheffMatrix implements Matrix {
         } else {
             return false;
         }
+
         return true;
     }
 
@@ -157,33 +166,32 @@ public class MartusheffMatrix implements Matrix {
         return matrix;
     }
 
-
-    
     /**
      * Entry point for matrix testing.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         int[][] data1 = new int[0][0];
         int[][] data2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] data3 = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
         int[][] data4 = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
-        
+
         Matrix m1 = new MartusheffMatrix(data1);
         Matrix m2 = new MartusheffMatrix(data2);
         Matrix m3 = new MartusheffMatrix(data3);
         Matrix m4 = new MartusheffMatrix(data4);
 
-
-
         System.out.println("m1 --> Rows: " + m1.getRows() + " Columns: " + m1.getColumns());
         System.out.println("m2 --> Rows: " + m2.getRows() + " Columns: " + m2.getColumns());
         System.out.println("m3 --> Rows: " + m3.getRows() + " Columns: " + m3.getColumns());
-        
+
+        System.out.println();
+
         //check for reference issues
         System.out.println("m2 -->\n" + m2);
-        data2[1][1] = 101;
+        data2[1][1] = 10;
         System.out.println("m2 -->\n" + m2);
 
         //test equals
@@ -193,20 +201,21 @@ public class MartusheffMatrix implements Matrix {
         System.out.println("m2==m2: " + m2.equals(m2));                 //true
         System.out.println("m2==m3: " + m2.equals(m3));                 //false
         System.out.println("m3==m4: " + m3.equals(m4));                 //true
-        
+
+        System.out.println();
+
         //test operations (valid)
         System.out.println("2 * m2:\n" + m2.scale(2));
         System.out.println("m2 + m3:\n" + m2.plus(m3));
         System.out.println("m2 - m3:\n" + m2.minus(m3));
-        
+
         //not tested... multiply(). you know what to do.
-        
+        System.out.println("m2 * m3:\n" + m2.multiply(m3));
+
         //test operations (invalid)
-        //System.out.println("m1 + m2" + m1.plus(m2));
-        //System.out.println("m1 - m2" + m1.minus(m2));
+        System.out.println("m1 + m2\n" + m1.plus(m2));
+        System.out.println("m1 - m2\n" + m1.minus(m2));
     }
-
-
 
 
 }
