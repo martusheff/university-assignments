d91f403cd9bccc779c37b02eaaae7e69ad64b6e8
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-29 12:31:54 -0700

    SER222 - Module 3, Stack Implementation, console UI

---
+++ SER222/Modules/Module 3/src/FixedCapacityStringStack.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class FixedCapacityStringStack {
+
+    private String[] data;
+    private int n = 0;
+
+    public FixedCapacityStringStack(int cap) {
+        data = new String[cap];
+    }
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public void push(String item) {
+        data[n++] = item;
+    }
+
+    public String pop() {
+        return data[--n];
+    }
+
+    public static void uiStackInteractor() {
+        Scanner s = new Scanner(System.in);
+        boolean inUse = true;
+        System.out.println("Enter 'q' at any time to quit.");
+        System.out.println("Let's start by creating a stack.\nWhat's the size of your fixed stack?");
+        int stackSize = s.nextInt();
+        FixedCapacityStringStack stack = new FixedCapacityStringStack(stackSize);
+        System.out.println("Stack of size '" + stackSize + "' created.");
+        System.out.println("Push/Pop from stack -- ex. 'Push {entry}' or 'Pop'.");
+
+        while (inUse) {
+            Scanner sc = new Scanner(System.in);
+            String response = sc.nextLine();
+            System.out.println(response);
+            if(response == "q") {
+                inUse = false;
+            }
+            if(response == "Pop") {
+                System.out.println("Your popped string is: " + stack.pop() + ".");
+            } else {
+                String valToPush = handleInput(response);
+                stack.push(valToPush);
+                System.out.println("Successfully pushed '" + valToPush + "' to stack." );
+            }
+
+        }
+    }
+
+    public static String handleInput(String input) {
+        String[] parts = input.split(" ");
+
+        return parts[1];
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+        uiStackInteractor();
+
+
+    }
+}
